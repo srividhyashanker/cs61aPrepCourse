341d4868b225992bb81bfa3b6384b572deae90e3
Srividhya Shanker <srividhyashanker@Srividhyas-MacBook-Air-6.local>
2016-12-21 10:40:15 -0800

    Initial commit

---
+++ Chapter5/chapter5.rb
@@ -0,0 +1,16 @@
+
+      
+puts 'Hi! What\'s your first name?'
+firstName = gets.chomp
+puts 'What\'s your middle name?'
+midName = gets.chomp
+puts 'What\'s your last name?'
+lastName = gets.chomp
+puts 'Hi ' + firstName + ' '+ midName + ' '+ lastName + '!' 
+
+puts 'Hey What\'s your favorite number?'
+num = gets.chomp
+numInt = faveNum.to_i + 1
+puts numInt.to_s + ' is the bigger and better favorite number!'
+
+
